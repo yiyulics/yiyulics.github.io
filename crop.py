70d4eb1039de982ae3b6896fd4df949a9b50c248
Yiyu Li <yiyuli36@foxmail.com>
2025-06-26 12:44:06 +0800

    update iccv

--- crop.py
+++ crop.py
@@ -2,4 +2,4 @@ import cv2
 
 if __name__ == "__main__":
     image = cv2.imread("images/sehdr.jpg")
-    cv2.imwrite("images/cropped_sehdr.jpg", image[300:-250, :])
+    cv2.imwrite("images/cropped_sehdr.jpg", image[150:-800, 300:-300])
